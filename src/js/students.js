d4eb42e3300acd471e8f37cca7c731e5f259b699
Nataliya Bondarenko <nataliyabondarenko@MacBook-Pro-Nataliya.local>
2020-11-10 00:10:39 +0200

    0010

---
+++ src/js/students.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    name: 'Александр Козлов',
+    src: '../images/students-photo/kozlov.jpg',
+    description: 'Тимлид команды SNSVS, Мозг',
+    info: 'В свободное время борется с молниями',
+  },
+  {
+    name: 'Александр Миронович',
+    src: '../images/students-photo/myronovych.jpg',
+    description: 'Участник команды SNSVS',
+    info: 'Молчалив пока занят делом. Серьезный финансист',
+  },
+  {
+    name: 'Сергей Микетеек',
+    src: '../images/students-photo/mikiteek.jpg',
+    description: 'Участник команды SNSVS',
+    info: 'Сверхмощный студент - учиться сразу в трех группах: js, react, node',
+  },
+  {
+    name: 'Виталий Горбенко',
+    src: '../images/students-photo/horbenko.jpg',
+    description: 'Участник команды SNSVS',
+    info:
+      'Предприимчив настолько, что успел заработать денег во время проекта пока все учились. Любит шуточки шутить',
+  },
+  {
+    name: 'Команда SNSVS',
+    src: '../images/students-photo/group-photo.jpg',
+    description: 'Все участники команды SNSVS',
+    info: 'Два дня плодотворно трудились над проектом',
+  },
+  {
+    name: 'Наталья Бондаренко',
+    src: '../images/students-photo/bondarenko.jpg',
+    description: 'Участница команды SNSVS',
+    info:
+      'Не придумала, что про себя написать...Но, точно в восторге от команды и всей группы',
+  },
+];
