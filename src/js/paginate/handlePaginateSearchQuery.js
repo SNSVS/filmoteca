9dda9259573baeae157a0cc24c0a233a55d216e5
mikiteek <mikiteek@gmail.com>
2020-11-10 13:09:16 +0200

    Implement active button pagination, fixed reload

--- src/js/paginate/handlePaginateSearchQuery.js
+++ src/js/paginate/handlePaginateSearchQuery.js
@@ -3,6 +3,8 @@ import {handleStateBtn, generatePagesBtn} from "./btnHandler";
 import {clearEventsListenersPagesTranding} from "./toggleListenersPagesTranding";
 import {clearListenersPagesStorage} from "./toggleListenersStoragePages"
 import {addEventListenersPagesSearch} from "./toggleListenersPagesSearch";
+import btnActiveClassToggle from './btnActiveClassToggle';
+import fetchQueryService from '../services/fetchQueryService';
 
 const handlePaginationSearchQuery = (response) => {
   clearEventsListenersPagesTranding();
@@ -10,6 +12,7 @@ const handlePaginationSearchQuery = (response) => {
   refsPaginate.pagesList.innerHTML = "";
   handleStateBtn(response);
   generatePagesBtn(response);
+  btnActiveClassToggle(fetchQueryService.getPage(), fetchQueryService.getPrevPage());
   addEventListenersPagesSearch();
 }
 
--- src/js/paginate/handlePaginateStorage.js
+++ src/js/paginate/handlePaginateStorage.js
@@ -3,10 +3,15 @@ import refsPaginate from '../refsPaginat';
 import {handleStateBtn, generatePagesBtn} from "./btnHandler"
 import {clearEventsListenersPagesSearch} from "./toggleListenersPagesSearch"
 import {clearEventsListenersPagesTranding} from "./toggleListenersPagesTranding"
-import {addListenersPagesStorage} from "./toggleListenersStoragePages"
+import {addListenersPagesStorage} from "./toggleListenersStoragePages";
+import btnActiveClassToggle from './btnActiveClassToggle';
+import fetchQueryService from '../services/fetchQueryService';
 
 
 const handlePaginateStorage = (moviesArray) => {
+  if (!moviesArray) {
+    return;
+  }
   clearEventsListenersPagesTranding();
   clearEventsListenersPagesSearch();
   // console.log("come to handle paginate");
@@ -21,6 +26,7 @@ const handlePaginateStorage = (moviesArray) => {
   storagePaginateService.showContent();
   handleStateBtn(response);
   generatePagesBtn(response);
+  btnActiveClassToggle(fetchQueryService.getPage(), fetchQueryService.getPrevPage());
   addListenersPagesStorage();
 }
 
