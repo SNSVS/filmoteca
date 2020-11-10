45c045eb6160b484de431f7b09d60a0f5f18f13b
AlexKozzlov <alexandr.kozlov1120@gmail.com>
2020-11-10 15:08:09 +0200

    add animation for error messege

--- src/js/studentOneCardMarkap.js
+++ src/js/studentOneCardMarkap.js
@@ -2,22 +2,22 @@ import refs from './refs.js';
 import studentsData from '../data/students';
 import studetnOneCardTpl from '../templates/student.hbs';
 
-// refs.movies.addEventListener('click', oneStudetnCardHandler);
+refs.movies.addEventListener('click', oneStudetnCardHandler);
 
-// function oneStudetnCardHandler(e) {
-//   setTimeout(() => {
-//     if (e.target === e.currentTarget) {
-//       return;
-//     } else {
-//       let id = +e.target.id;
-//       console.log();
-//       const targetStudetnArray = studentsData.find(function (student) {
-//         return student.id === id;
-//       });
-//       const oneStudentMarkap = studetnOneCardTpl(targetStudetnArray);
-//       refs.movies.innerHTML = oneStudentMarkap;
-//       refs.movies.addEventListener('click', oneStudetnCardHandler);
-//       console.log(oneStudentMarkap);
-//     }
-//   }, 100);
-// }
+function oneStudetnCardHandler(e) {
+  setTimeout(() => {
+    if (e.target === e.currentTarget) {
+      return;
+    } else {
+      let id = +e.target.id;
+      console.log();
+      const targetStudetnArray = studentsData.find(function (student) {
+        return student.id === id;
+      });
+      const oneStudentMarkap = studetnOneCardTpl(targetStudetnArray);
+      refs.movies.innerHTML = oneStudentMarkap;
+      refs.movies.addEventListener('click', oneStudetnCardHandler);
+      console.log(oneStudentMarkap);
+    }
+  }, 100);
+}
