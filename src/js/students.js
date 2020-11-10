35d823e475f3578c941c93773fd8e012aaf73ac9
Nataliya Bondarenko <nataliyabondarenko@MacBook-Pro-Nataliya.local>
2020-11-10 18:04:04 +0200

    img update

--- src/js/students.js
+++ src/js/students.js
@@ -1,29 +1,36 @@
+import myronovychImg from '../images/studentsphoto/myronovych.jpg';
+import kozlovImg from '../images/studentsphoto/kozlov4.jpg';
+import mikiteekImg from '../images/studentsphoto/mikiteek2.jpg';
+import horbenkoImg from '../images/studentsphoto/horbenko.jpg';
+import groupphotoImg from '../images/studentsphoto/groupphoto.jpg';
+import bondarenkoImg from '../images/studentsphoto/bondarenko.jpg';
+
 export default [
   {
     id: 1,
     name: 'Александр Миронович',
-    src: 'images/studentsphoto/myronovych.jpg',
+    src: myronovychImg,
     description: 'Участник команды SNSVS',
     info: 'Молчалив пока занят делом. Серьезный финансист',
   },
   {
     id: 2,
     name: 'Александр Козлов',
-    src: './images/studentsphoto/kozlov2.jpg',
+    src: kozlovImg,
     description: 'Тимлид команды SNSVS, Мозг',
     info: 'В свободное время борется с молниями',
   },
   {
     id: 3,
     name: 'Сергей Микетеек',
-    src: './images/studentsphoto/mikiteek2.jpg',
+    src: mikiteekImg,
     description: 'Участник команды SNSVS',
     info: 'Сверхмощный студент - учиться сразу в трех группах: js, react, node',
   },
   {
     id: 4,
     name: 'Виталий Горбенко',
-    src: '../images/studentsphoto/horbenko.jpg',
+    src: horbenkoImg,
     description: 'Участник команды SNSVS',
     info:
       'Предприимчив настолько, что успел заработать денег во время проекта пока все учились. Любит шуточки шутить',
@@ -31,7 +38,7 @@ export default [
   {
     id: 5,
     name: 'Команда SNSVS',
-    src: './images/studentsphoto/groupphoto.jpg',
+    src: groupphotoImg,
     description: 'Все участники команды SNSVS',
     info:
       'Два дня плодотворно трудились над проектом. Ночами не спали. Репету заслушали до дыр',
@@ -39,9 +46,8 @@ export default [
   {
     id: 6,
     name: 'Наталья Бондаренко',
-    src: './images/studentsphoto/bondarenko.jpg',
+    src: bondarenkoImg,
     description: 'Участница команды SNSVS',
-    info:
-      'Не придумала, что про себя написать...Но, точно в восторге от команды и всей группы',
+    info: 'В восторге от команды и всей группы',
   },
 ];
