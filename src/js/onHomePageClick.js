c757ba35711b09a61709bc4aacd02f863827ad17
mikiteek <mikiteek@gmail.com>
2020-11-10 14:28:48 +0200

    Fixed bug with search input, and click by cards

--- src/js/onHomePageClick.js
+++ src/js/onHomePageClick.js
@@ -1,9 +1,16 @@
 import refs from './refs';
 import getTrendingMovies from './requestsToServer/getTrendingMovies';
+import searchForm from '../templates/searchForm.hbs';
+import {onMovies} from "./myronovych"
+import debouncedInputQuery from './homePage';
 
 const handlerHomePageClick = (event) => {
   event.preventDefault();
   getTrendingMovies();
+  refs.movies.classList.add('movies__list');
+  refs.movies.addEventListener('click', onMovies);
+  refs.searchBtn.innerHTML = searchForm();
+  refs.searchBtn.addEventListener("input", debouncedInputQuery);
 }
 
 const onHomePageClick = () => {
